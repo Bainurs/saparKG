5d30d8d8c7b5e5e27c9506947244d6b63b1cb7ac
Frogy <nurs.axe@gmail.com>
2021-06-10 18:18:26 +0600

    calculator create

--- js/calculate.js
+++ js/calculate.js
@@ -1,102 +1,143 @@
- let myMap;
-let suggestViewFrom, suggestViewTo;
-let from, to;
-let multiRouteModel, routeTypeSelector;
-let autoRouteItem, masstransitRouteItem, pedestrianRouteItem;
-
-document.getElementById('run').addEventListener('click', function () {
-  from = document.getElementById('from').value;
-  console.log('from ', from);
-  to = document.getElementById('to').value;
-  console.log('to ', to);
- // Создаем модель мультимаршрута.
+let suggestTaxiFrom, suggestTaxiTo, suggestParcelFrom, suggestParcelTo;
 
 
-  // ymaps.route([from, to])
-  //       .then(function (route) {
-  //           myMap.geoObjects.add(route);
-  //           console.log('route.editor.getLenght() ',route.properties.get("distance"));
-  //       },
-  //       function (error) {
-  //           alert('Возникла ошибка: ' + error.message);
-  //       });
-
-});       
-function init () {
-    multiRouteModel = new ymaps.multiRouter.MultiRouteModel([
-      from,
-      to
-    ], {
-      // Путевые точки можно перетаскивать.
-      // Маршрут при этом будет перестраиваться.
-      wayPointDraggable: true,
-      boundsAutoApply: true
-    });
+ymaps.ready(function() {
+    suggestTaxiFrom = new ymaps.SuggestView('fromTaxi');
+    suggestTaxiTo = new ymaps.SuggestView('toTaxi');
     
-    // Создаём выпадающий список для выбора типа маршрута.
-    routeTypeSelector = new ymaps.control.ListBox({
-      data: {
-          content: 'Как добраться'
-      },
-      items: [
-          new ymaps.control.ListBoxItem({data: {content: "Авто"},state: {selected: true}}),
-          new ymaps.control.ListBoxItem({data: {content: "Общественным транспортом"}}),
-          new ymaps.control.ListBoxItem({data: {content: "Пешком"}})
-      ],
-      options: {
-          itemSelectOnClick: false
+    suggestParcelFrom = new ymaps.SuggestView('fromParcel');
+    suggestParcelTo = new ymaps.SuggestView('toParcel');
+});
+
+document.getElementById('calcTaxi').addEventListener('click', function() {
+    calculateTaxi();
+  });
+  document.getElementById('calcParcel').addEventListener('click', function() {
+    calculateParcel();
+  });
+
+function calculateTaxi() {
+    ymaps.ready(init);
+    function init() {
+      // Стоимость за километр.S
+      var DELIVERY_TARIFF = 15,
+          BOARDING_COST = 70,
+      // Минимальная стоимость.
+          MINIMUM_COST = 70,
+          routePanelControl = new ymaps.control.RoutePanel({
+            options: {
+                types: {auto: true}
+            }
+        });
+      let myMap = new ymaps.Map('map', {
+        center: [60.906882, 30.067233],
+        zoom: 9,
+        controls: []
+      }); 
+      let fromAddress = document.getElementById('fromTaxi').value,
+          toAddress = document.getElementById('toTaxi').value;
+    
+      // Если вы хотите задать неизменяемую точку "откуда", раскомментируйте код ниже.
+      routePanelControl.routePanel.state.set({
+          fromEnabled: true,
+          toEnabled: true,
+          from: fromAddress,
+          to: toAddress
+       });
+       
+       myMap.controls.add(routePanelControl);
+      // Получим ссылку на маршрут.
+      routePanelControl.routePanel.getRouteAsync().then(function (route) {
+    
+          // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
+          route.model.setParams({results: 1}, true);
+    
+          // Повесим обработчик на событие построения маршрута.
+          route.model.events.add('requestsuccess', function () {
+              var activeRoute = route.getActiveRoute();
+              if (activeRoute) {
+                  // Получим протяженность маршрута.
+                  var length = route.getActiveRoute().properties.get("distance"),
+                      time = route.getActiveRoute().properties.get("duration"),
+                  // Вычислим стоимость доставки.
+                      price = calculate(Math.round(length.value / 1000));
+                  // Создадим макет содержимого балуна маршрута.
+                     console.log(length.text);
+                     
+                     document.getElementById('timeTaxi').value = time.text
+                     document.getElementById('priceTaxi').value = `${price} сом`;
+              }
+          });
+    
+      });
+      // Функция, вычисляющая стоимость доставки.
+      function calculate(routeLength) {
+          return Math.max(routeLength * DELIVERY_TARIFF + BOARDING_COST, MINIMUM_COST);
       }
-    }),
-    // Получаем прямые ссылки на пункты списка.
-    autoRouteItem = routeTypeSelector.get(0),
-    masstransitRouteItem = routeTypeSelector.get(1),
-    pedestrianRouteItem = routeTypeSelector.get(2);
-    
-    // Подписываемся на события нажатия на пункты выпадающего списка.
-    autoRouteItem.events.add('click', function (e) { changeRoutingMode('auto', e.get('target')); });
-    masstransitRouteItem.events.add('click', function (e) { changeRoutingMode('masstransit', e.get('target')); });
-    pedestrianRouteItem.events.add('click', function (e) { changeRoutingMode('pedestrian', e.get('target')); });
-    
-    ymaps.modules.require([
-    'MultiRouteCustomView'
-    ], function (MultiRouteCustomView) {
-    // Создаем экземпляр текстового отображения модели мультимаршрута.
-    // см. файл custom_view.js
-    new MultiRouteCustomView(multiRouteModel);
-    });
-
-  // Создаем карту с добавленной на нее кнопкой.
-  var myMap = new ymaps.Map('map', {
-          center: [42.86667, 74.56667],
-          zoom: 10,
-          controls: [routeTypeSelector]
-      }, {
-          buttonMaxWidth: 300
-      }),
-
-      // Создаем на основе существующей модели мультимаршрут.
-      multiRoute = new ymaps.multiRouter.MultiRoute(multiRouteModel, {
-        // Путевые точки можно перетаскивать.
-        // Маршрут при этом будет перестраиваться.
-        wayPointDraggable: true,
-        boundsAutoApply: true
-    });
-
-  // Добавляем мультимаршрут на карту.
-  myMap.geoObjects.add(multiRoute);
-
-  function changeRoutingMode(routingMode, targetItem) {
-      multiRouteModel.setParams({ routingMode: routingMode }, true);
-
-      // Отменяем выбор элементов.
-      autoRouteItem.deselect();
-      masstransitRouteItem.deselect();
-      pedestrianRouteItem.deselect();
-
-      // Выбираем элемент и закрываем список.
-      targetItem.select();
-      routeTypeSelector.collapse();
+      
+    }
   }
-} 
-ymaps.ready(init);
+  
+  function calculateParcel() {
+    ymaps.ready(init);
+    function init() {
+      // Стоимость за километр.S
+      var DELIVERY_TARIFF = 15,
+          BOARDING_COST = 100,
+          weightParcel = document.getElementById('weightParcel').value,
+      // Минимальная стоимость.
+          MINIMUM_COST = 100,
+          routePanelControl = new ymaps.control.RoutePanel({
+            options: {
+                types: {auto: true}
+            }
+        });
+      let myMap = new ymaps.Map('map', {
+        center: [60.906882, 30.067233],
+        zoom: 9,
+        controls: []
+      }); 
+      
+      let fromAddress = document.getElementById('fromParcel').value,
+          toAddress = document.getElementById('toParcel').value;
+  
     
+      // Если вы хотите задать неизменяемую точку "откуда", раскомментируйте код ниже.
+      routePanelControl.routePanel.state.set({
+          fromEnabled: true,
+          toEnabled: true,
+          from: fromAddress,
+          to: toAddress
+       });
+       
+       myMap.controls.add(routePanelControl);
+      // Получим ссылку на маршрут.
+      routePanelControl.routePanel.getRouteAsync().then(function (route) {
+    
+          // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
+          route.model.setParams({results: 1}, true);
+    
+          // Повесим обработчик на событие построения маршрута.
+          route.model.events.add('requestsuccess', function () {
+              var activeRoute = route.getActiveRoute();
+              if (activeRoute) {
+                  // Получим протяженность маршрута.
+                  var length = route.getActiveRoute().properties.get("distance"),
+                      time = route.getActiveRoute().properties.get("duration"),
+                  // Вычислим стоимость доставки.
+                      price = calculate(Math.round(length.value / 1000));
+                  // Создадим макет содержимого балуна маршрута.
+                     console.log(length.text);
+                     document.getElementById('timeParcel').value = time.text
+                     document.getElementById('priceParcel').value = `${price} сом`;
+              }
+          });
+    
+      });
+      // Функция, вычисляющая стоимость доставки.
+      function calculate(routeLength) {
+          return Math.max(routeLength * DELIVERY_TARIFF + BOARDING_COST + (parseInt(weightParcel) * 3), MINIMUM_COST);
+      }
+      
+    }
+  }
