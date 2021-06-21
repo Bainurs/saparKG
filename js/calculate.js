e029e60941295027482cac2f1d5fca3c6f769483
Frogy <nurs.axe@gmail.com>
2021-06-21 17:20:48 +0600

    add calculate for taxi and parcelTaxi

--- js/calculate.js
+++ js/calculate.js
@@ -2,15 +2,20 @@ let suggestTaxiFrom, suggestTaxiTo, suggestParcelFrom, suggestParcelTo;
 
 
 ymaps.ready(function() {
-    suggestTaxiFrom = new ymaps.SuggestView('fromTaxi');
+    suggestTaxiFrom = new ymaps.SuggestView('fromTaxi',{provider: 'yandex#map', kind: ''});
     suggestTaxiTo = new ymaps.SuggestView('toTaxi');
     
     suggestParcelFrom = new ymaps.SuggestView('fromParcel');
     suggestParcelTo = new ymaps.SuggestView('toParcel');
+
+    
 });
 
 document.getElementById('calcTaxi').addEventListener('click', function() {
+    console.log('suggestTaxiFrom : ',suggestTaxiFrom)
+
     calculateTaxi();
+
   });
   document.getElementById('calcParcel').addEventListener('click', function() {
     calculateParcel();
@@ -19,24 +24,32 @@ document.getElementById('calcTaxi').addEventListener('click', function() {
 function calculateTaxi() {
     ymaps.ready(init);
     function init() {
-      // Стоимость за километр.S
-      var DELIVERY_TARIFF = 15,
-          BOARDING_COST = 70,
-      // Минимальная стоимость.
-          MINIMUM_COST = 70,
-          routePanelControl = new ymaps.control.RoutePanel({
+        // Стоимость за километр.S
+        var DELIVERY_TARIFF = 3,
+            BOARDING_COST = 70,
+        // Минимальная стоимость.
+            MINIMUM_COST = 70,
+            routePanelControl = new ymaps.control.RoutePanel({
             options: {
                 types: {auto: true}
             }
         });
-      let myMap = new ymaps.Map('map', {
+        let myMap = new ymaps.Map('map', {
         center: [60.906882, 30.067233],
         zoom: 9,
         controls: []
-      }); 
+        }),
+        searchControl = new ymaps.control.SearchControl({
+            options: {
+                provider: 'yandex#search',
+                position: {top: -45},
+                
+            }
+        });
+      
       let fromAddress = document.getElementById('fromTaxi').value,
           toAddress = document.getElementById('toTaxi').value;
-    
+      
       // Если вы хотите задать неизменяемую точку "откуда", раскомментируйте код ниже.
       routePanelControl.routePanel.state.set({
           fromEnabled: true,
@@ -45,29 +58,29 @@ function calculateTaxi() {
           to: toAddress
        });
        
-       myMap.controls.add(routePanelControl);
+      myMap.controls.add(routePanelControl).add(searchControl);
       // Получим ссылку на маршрут.
       routePanelControl.routePanel.getRouteAsync().then(function (route) {
     
-          // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
-          route.model.setParams({results: 1}, true);
-    
-          // Повесим обработчик на событие построения маршрута.
-          route.model.events.add('requestsuccess', function () {
-              var activeRoute = route.getActiveRoute();
-              if (activeRoute) {
-                  // Получим протяженность маршрута.
-                  var length = route.getActiveRoute().properties.get("distance"),
-                      time = route.getActiveRoute().properties.get("duration"),
-                  // Вычислим стоимость доставки.
-                      price = calculate(Math.round(length.value / 1000));
-                  // Создадим макет содержимого балуна маршрута.
-                     console.log(length.text);
-                     
-                     document.getElementById('timeTaxi').value = time.text
-                     document.getElementById('priceTaxi').value = `${price} сом`;
-              }
-          });
+        // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
+        route.model.setParams({results: 1}, true);
+
+        // Повесим обработчик на событие построения маршрута.
+        route.model.events.add('requestsuccess', function () {
+            var activeRoute = route.getActiveRoute();
+            if (activeRoute) {
+                // Получим протяженность маршрута.
+                var length = route.getActiveRoute().properties.get("distance"),
+                    time = route.getActiveRoute().properties.get("duration"),
+                // Вычислим стоимость доставки.
+                    price = calculate(Math.round(length.value / 1000));
+                // Создадим макет содержимого балуна маршрута.
+                    console.log(length.text);
+                    
+                    document.getElementById('timeTaxi').value = time.text
+                    document.getElementById('priceTaxi').value = `${price} сом`;
+            }
+        });
     
       });
       // Функция, вычисляющая стоимость доставки.
@@ -82,11 +95,11 @@ function calculateTaxi() {
     ymaps.ready(init);
     function init() {
       // Стоимость за километр.S
-      var DELIVERY_TARIFF = 15,
-          BOARDING_COST = 100,
+      var DELIVERY_TARIFF = 3,
+          BOARDING_COST = 70,
           weightParcel = document.getElementById('weightParcel').value,
       // Минимальная стоимость.
-          MINIMUM_COST = 100,
+          MINIMUM_COST = 70,
           routePanelControl = new ymaps.control.RoutePanel({
             options: {
                 types: {auto: true}
@@ -136,8 +149,8 @@ function calculateTaxi() {
       });
       // Функция, вычисляющая стоимость доставки.
       function calculate(routeLength) {
-          return Math.max(routeLength * DELIVERY_TARIFF + BOARDING_COST + (parseInt(weightParcel) * 3), MINIMUM_COST);
+          return Math.max(routeLength * (DELIVERY_TARIFF + parseInt(weightParcel)) + BOARDING_COST, MINIMUM_COST);
       }
       
     }
-  }
+  } 
